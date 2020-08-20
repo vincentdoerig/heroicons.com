969d206f61e68a36cb61ad4e709eb88f235c45a6
Brad Cornes <bradlc41@gmail.com>
2020-08-20 11:43:13 +0100

    Update icon block styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   experimental: {
@@ -18,18 +19,35 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        'twitter-blue': '#1DA1F2'
+        'twitter-blue': '#1DA1F2',
       },
       opacity: {
         10: '0.1',
         12: '0.12',
         20: '0.2',
-        40: '0.4'
-      }
+        40: '0.4',
+      },
     },
   },
   variants: {
-    opacity: ['responsive', 'hover', 'focus', 'group-hover', 'focus-within'],
-    textColor: ['responsive', 'hover', 'focus', 'group-hover', 'focus-within'],
-  }
+    opacity: [
+      'responsive',
+      'hover',
+      'focus',
+      'group-hover',
+      'focus-within',
+      'group-focus-within',
+    ],
+  },
+  plugins: [
+    plugin(({ addVariant, e }) => {
+      addVariant('group-focus-within', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.group:focus-within .${e(
+            `group-focus-within${separator}${className}`
+          )}`
+        })
+      })
+    }),
+  ],
 }
