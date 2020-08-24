cbbec059b9aa8262bb3b5e034697eef26091a051
Brad Cornes <bradlc41@gmail.com>
2020-08-24 15:44:02 +0100

    Add page title and description

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,19 @@
 import '../css/main.css'
+import Head from 'next/head'
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Heroicons</title>
+        <meta
+          name="description"
+          content="Beautiful hand-crafted SVG icons, by the makers of Tailwind CSS."
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
