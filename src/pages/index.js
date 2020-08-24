4d82d67175ec1e4130141cda80687f1bea724d78
Brad Cornes <bradlc41@gmail.com>
2020-08-24 17:42:10 +0100

    Add top border to footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -515,7 +515,7 @@ function Header() {
 
 function Footer() {
   return (
-    <footer className="bg-gray-50 py-12 md:py-14 px-4 sm:px-6 lg:px-16 text-sm leading-5">
+    <footer className="bg-gray-50 border-t border-gray-200 py-12 md:py-14 px-4 sm:px-6 lg:px-16 text-sm leading-5">
       <div className="max-w-container mx-auto text-center space-y-6 md:space-y-0 md:text-left md:flex">
         <div className="space-y-6 md:space-y-0 md:space-x-10 flex flex-col items-center md:flex-row">
           <div className="flex items-center space-x-2">
